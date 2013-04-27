2eab9c8ad4d5110e573e88e4f93e5ceefc37f001
nhvtgd <viettran@mit.edu>
2013-04-27 12:05:21 -0400

    fix bugs

--- src/mit/edu/stemplusplus/ProjectActivity.java
+++ src/mit/edu/stemplusplus/ProjectActivity.java
@@ -88,6 +88,7 @@ public class ProjectActivity extends StemPlusPlus {
         stepAdapter = new CustomizedStepAdapterforPosting(ProjectActivity.this, steps);
         stepListView.setAdapter(stepAdapter);
         
+        
        
     }
     private OnClickListener onclick = new OnClickListener(){
